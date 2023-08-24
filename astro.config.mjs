194980469f7da4d8ff41f3b77a1f43bc21d8274a
eryue0220 <eryue0220@gmail.com>
2023-08-25 00:07:02 +0800

    feat: update page to astro

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    drafts: true,
+  },
+  integrations: [
+    mdx({ drafts: true }),
+    tailwind(),
+  ],
+  site: 'https://eryue0220.github.io/',
+});
